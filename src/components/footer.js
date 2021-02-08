3103826e64749f6ed14034bf281d2ec1b9f045e7
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-02-08 15:18:29 -0700

    change footer text

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 const Footer = ({ copyrights }) => (
   <footer>
@@ -12,18 +12,17 @@ const Footer = ({ copyrights }) => (
     ) : (
       <>
         <span className="footerCopyrights">
-          © 2019 Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </span>
-        <span className="footerCopyrights">
-          Starter created by <a href="https://radoslawkoziel.pl">panr</a>
+          © 2021 Built by{" "}
+          <a href="https://github.com/nicholasrokosz">Nick Rokosz</a> with{" "}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
         </span>
       </>
     )}
   </footer>
-)
+);
 
 Footer.propTypes = {
   copyrights: PropTypes.string,
-}
+};
 
-export default Footer
+export default Footer;
