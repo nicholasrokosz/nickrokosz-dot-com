43108f3aebc43a19ae133ace16f4bd20949d9e91
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-02-10 00:49:07 -0700

    add children prop

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -1,11 +1,12 @@
 import React from "react";
 import style from "../styles/ExternalLink.module.css";
 
-const ExternalLink = ({ url, text, icon }) => {
+const ExternalLink = ({ url, text, children }) => {
   return (
     <a href={url} target="_blank">
       {text}
-      <img src={`logos/${icon}.svg`} className={style.icon} />
+      {/* <img src={`logos/${icon}.svg`} className={style.icon} /> */}
+      <div className={style.icon}>{children}</div>
     </a>
   );
 };
